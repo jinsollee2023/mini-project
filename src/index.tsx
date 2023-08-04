d5480e764d0ffea1cfa12a635ccc26d902be82c3
Lee Jinsol <jins2ol@naver.com>
2023-08-04 16:03:05 +0900

    add dev branch

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Layout from "./common/Layout";
-import GlobalStyle from "../GlobalStyle";
+import GlobalStyle from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
