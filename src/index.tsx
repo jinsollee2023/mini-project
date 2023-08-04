1992a957631afc4b26392b794fc2630523cb2734
Lee Jinsol <jins2ol@naver.com>
2023-08-04 11:38:27 +0900

    add Global Style

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Layout from "./common/Layout";
+import GlobalStyle from "../GlobalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Layout>
       <App />
     </Layout>
